1d436f47bff8ca4afaf1bdef00461e2b460c4dd3
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:47:09 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// simple calculator:
+
+const num1 = 20 ;
+const num2 = 10 ;
+console.log("The Answer of SUM is :", num1+num2);
+console.log("The Answer OF SUBRACTION is :", num1-num2);
+console.log("The Answer of DIVISION is :", num1/num2);
+console.log("The Answer of MULTIPLICATION is :", num1*num2);
